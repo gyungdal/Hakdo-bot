f949f7120944974f5f525dcc9e8b6dcf22271e6b
Gyeongsik_Kim <9274aa@gmail.com>
2017-07-18 17:06:44 +0900

    #, node app.js
    #node app.js
    node app.js
    node app.js  API
    
    
    Former-commit-id: ff205870da495b1ae9b1e2501250c4a538d7fdf7

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const cluster = require('cluster');
 const fs = require('fs');
 const botToken = require('./token').token;
 const weatherApiKey = require('./token').weather;
+const apiaiKey = require('./token').apiai;
 console.log(botToken);
 
 if (cluster.isMaster) {
 	var worker = cluster.fork();
+	
+	String.prototype.replaceAll = function(target, replacement) {
+		return this.split(target).join(replacement);
+	};
 	
 	worker.on('message', function (message) {
 		if(message == 'start'){
@@ -42,9 +47,12 @@ if (cluster.isMaster) {
 if (cluster.isWorker) {
 	const Discord = require('discord.js');
 	const client = new Discord.Client();
-	var admins = ["253024615285129227"];
+	const botId = require('./token').botId;
+	const uuidFunction = require('uuid-v4');
+	const express = require('express');
+	const mainduuid = uuidFunction();
+	var admins = [require('./token').adminId];
 	var exec = require('child_process').exec;
-	
 	process.on('message', function(message) {
 		client.on('ready', () => {	
 			console.log(message);
@@ -59,31 +67,32 @@ if (cluster.isWorker) {
 		console.log('I am ready!');
 	});
 	
-	client.on('message', message => {
-		if(message.content.indexOf('h!weather') == 0){
-			const request = require('request');
-			let city = 'portland';
-			message.content = message.content.replace('  ', ' ');
-			let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + message.content.split(' ')[1] + '&appid=' + weatherApiKey;
-			request(url, function (err, response, body) {
-				if(err){
-					console.log('error:', error);
-				} else {
-					console.log(body);
-					body = JSON.parse(body);
-					const weather = body.weather[0].description;
-					const temp = body.main.temp - 273.15;
-					const humi = body.main.humidity;
-					const temp_min = body.main.temp_min - 273.15;
-					const temp_max = body.main.temp_max - 273.15;
-					const wind_speed = body.wind.speed;
-					message.reply(`<Warring!!! - BETA VERSION>\n\n<Weather>\nWEATHER : ${weather}\nTEMP : ${temp}\nHUMI : ${humi}`);
-					
-				}
-			});
-		}
-	});
+	//client.on('message', message => {
+	//	if(message.content.indexOf('h!weather') == 0){
+	//		const request = require('request');
+	//		let city = 'portland';
+	//		message.content = message.content.replace('  ', ' ');
+	//		let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + message.content.split(' ')[1] + '&appid=' + weatherApiKey;
+	//		request(url, function (err, response, body) {
+	//			if(err){
+	//				console.log('error:', error);
+	//			} else {
+	//				console.log(body);
+	//				body = JSON.parse(body);
+	//				const weather = body.weather[0].description;
+	//				const temp = body.main.temp - 273.15;
+	//				const humi = body.main.humidity;
+	//				const temp_min = body.main.temp_min - 273.15;
+	//				const temp_max = body.main.temp_max - 273.15;
+	//				const wind_speed = body.wind.speed;
+	//				message.reply(`<Warring!!! - BETA VERSION>\n\n<Weather>\nWEATHER : ${weather}\nTEMP : ${temp}\nHUMI : ${humi}`);
+	//				
+	//			}
+	//		});
+	//	}
+	//});
 
+	
 	client.on('message', message => {
 		//console.log(message);
 		if(message.content.indexOf('h!help') == 0){
@@ -108,6 +117,155 @@ if (cluster.isWorker) {
 			}
 		}
 	});
+	
+	
+	client.on('message', message => {
+		const userlist = message.mentions.users;
+		console.log(userlist);
+		userlist.forEach(function(user){
+			if(user.id == botId){
+				const str = message.content.substring(message.content.lastIndexOf('>') + 1);
+				console.log(str);
+				const request = require('request');   
+				
+		const storyId = require('./token').mind;
+		const url = "http://mindmap.ai:8000/v1/" + storyId;
+		var inputJsonObjectDataInit = {
+			"story_id": storyId,
+			"context": {
+				"conversation_id": mainduuid,
+				"information": {
+					"conversation_stack": [
+						{
+							"conversation_node": 'root',
+							"conversation_node_name": '루트노드'
+						}
+					],
+					"conversation_counter": 0,
+					"user_request_counter": 0,
+				},
+				"visit_counter": 0,
+				"reprompt": false,
+				"retrieve_field": false,
+				"message": null,
+				"keyboard": null,
+				"random": false,
+				"input_field": false,
+				"variables": null
+			},
+			"input": {
+				"text": str
+			}
+		};
+		 
+		// request 보내기
+		var json = '';
+		request({
+				url: url,
+				method: 'POST',
+				json: inputJsonObjectDataInit
+		 
+			},
+			// response 받기
+			function(error, response, body){
+				console.log("--------- response 시작 ----------");
+				console.log(body);
+				console.log("--------- response 끝 ----------");
+				json = body;
+		 
+				// 받은 텍스트보기
+				var outputTextArray = json["output"]["visit_nodes_text"];
+				console.log("outputTextArray: " + outputTextArray.toString());
+				for(var i=0 ; i < outputTextArray.length ; i++){
+					//실행된 모든 노드의 대답을 표시한다
+					console.log(outputTextArray[i]);
+				}
+		 
+		 
+				// ** 다시 보낼 payload 재가공하기
+				console.log("");
+				console.log("--------- 보낼 new_inputJsonObjectData 재가공 시작 ----------");
+				var new_inputtxt = str;  // 이부분만 재가공하여 처리하여 다시 메시지를 보내면 된다.
+				var new_context = json['context'];
+				var new_inputJsonObjectData = {
+					"story_id": storyId,
+					"context": new_context,
+					"input": {
+						"text": new_inputtxt
+					}
+		 
+				}
+				console.log("받은 context 지만 다시 보낼 context: " + JSON.stringify(new_context));  // 그대로 보내야지 변수들이 유지되어 mindmap이 잘 작동한다.
+				console.log("가공후 새롭게 보낼 new_inputtxt: " + new_inputtxt);
+				console.log("재가공된  'new_inputJsonObjectData' 이걸 다시 request를 만들어 보내면 된다. : " + JSON.stringify(new_inputJsonObjectData));
+				console.log("------------ 보낼 new_inputJsonObjectData 재가공하기 끝 ----------");
+				var json = '';
+				request({
+					url: url,
+					method: 'POST',
+					json: new_inputJsonObjectData
+
+				},
+					// response 받기
+					function(error, response, body){;
+						if(body["output"]["visit_nodes_name"][0] == '.mr'){
+							const urlencode = require('urlencode');
+							const request = require('request');
+							const naver_client_id = require('./token').naver_client_id;
+							const naver_client_secret = require('./token').naver_client_secret;
+							var searchURI = ("https://openapi.naver.com/v1/search/encyc.json?query=" + urlencode(str.trim())).trim();
+							console.log(searchURI);
+							const options = {
+								url: searchURI,
+								headers: { 
+											'X-Naver-Client-Id':naver_client_id, 
+											'X-Naver-Client-Secret': naver_client_secret,
+											"Content-Type" : "application/json; charset=utf-8"
+										}
+							};
+							request.get(options, function (error, res, body) {
+								if (!error) {
+									console.log(body);
+									const json = JSON.parse(body);
+									message.channel.send('<' + str + '>\n' + json["items"][0]['description'].replace(/<(?:.|\n)*?>/gm, ''));
+								}
+								console.log(error);
+							});
+						}else{
+							console.log(body);
+							json = body;
+							var outputTextArray = json["output"]["visit_nodes_text"];
+							console.log("outputTextArray: " + outputTextArray.toString());
+							for(var i=0 ; i < outputTextArray.length ; i++){
+								//실행된 모든 노드의 대답을 표시한다
+								console.log(outputTextArray[i]);
+							}
+							message.channel.send(json["output"]["text"][0]);
+						
+								
+						}
+				});
+			});
+					 
+				//var request = app.textRequest(str, {
+				//	sessionId: '1'
+				//});
+                //
+				//request.on('response', function(response) {
+				//	console.log(response);
+				//	message.reply(response.result.fulfillment.speech);
+				//});
+                //
+				//request.on('error', function(error) {
+				//	console.log(error);
+				//	message.reply(error);
+				//});
+                //
+				//request.end();
+			}
+		});
+	});
+	
 	
 	client.on('message', message => {
 		if(message.content.indexOf('h!adminList') == 0 & admins.indexOf(message.author.id) == 0){
